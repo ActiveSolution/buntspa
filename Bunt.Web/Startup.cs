98cc156742e9e931d2bd3ef1f1d12af9838a8af4
Oskar Dillén <oskar.dillen@activesolution.se>
2017-12-11 14:28:18 +0100

    Empty AccessBehavior

--- Bunt.Web/Startup.cs
+++ Bunt.Web/Startup.cs
@@ -35,6 +35,7 @@ namespace Bunt.Web
 
             services.AddSingleton<ILogger>(logger);
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AccessBehavior<,>));
             services.AddTransient<IConnectionFactory>(provider => new SqlConnectionFactory(Configuration.GetConnectionString("BuntDb")));
             services.AddDbContext<BuntDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BuntDb")));
             services.AddMediatR(typeof(ListaBuntladeStallen.Handler));
